48fb8883486509834d5b6ab91fade148d5c0fa81
jamie jamison <jamison@ucla.edu>
2015-02-24 14:15:07 -0800

    15Feb24jmj: commit todays c/c++ exercies.

---
+++ hello2.cxx
@@ -0,0 +1,36 @@
+/*
+ * hello2.cxx
+ * 
+ * Copyright 2015 jamison <jamison@SSGS02-PC>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+#include <iostream>
+using namespace std;   	// all names available
+using std::cout;		// makes cin available explicity
+using std::cin;  		// mkes cin available explicitly
+
+int main(int argc, char **argv)
+{
+	cout << "Hello World++!";
+	
+	return 0;
+}
+
